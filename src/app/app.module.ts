3b87701f2097003ae0b1c7ba7921637a98e45414
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2023-08-23 18:55:36 -0300

    agregando navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './books/book/book.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
