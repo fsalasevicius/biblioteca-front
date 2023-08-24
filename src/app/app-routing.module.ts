943ab13099a88ba848da9c3068cb9709ef801d2c
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2023-08-23 21:27:09 -0300

    Estilando Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './books/book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: BookComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
